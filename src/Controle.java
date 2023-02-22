a1eb2518ff92fdc962b9fc3ae5a7325d4a56993e
igorpardinho <pardinhoigor@gmail.com>
2023-02-22 16:41:25 -0300

    Initial commit

---
+++ src/Controle.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Controle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero: ");
+        int primeiroNumero = sc.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int segundoNumero = sc.nextInt();
+        try {
+            contar(primeiroNumero,segundoNumero);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+
+    }
+    static void contar(int numero1, int numero2) throws ParametrosInvalidosException{
+        if(numero1 > numero2){
+            throw new ParametrosInvalidosException();
+        } else {
+            int contagem = numero2 - numero1;
+
+            for (int i = 1; i <= contagem; i++){
+                System.out.println("imprimindo o numero " + i);
+            }
+        }
+    }
+}
